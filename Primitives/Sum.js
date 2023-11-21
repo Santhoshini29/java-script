5f2c605563e5af92e8d77ec49ec603b4d91df59e
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 03:23:00 +0530

    updated

--- Primitives/Sum.js
+++ Primitives/Sum.js
@@ -1,4 +1,10 @@
 function sumAll(startingindex,endingindex){
-
- 
+    var sum= 0;
+    for(var i=startingindex;i<endingindex;i++){
+        sum+=i;
+    }
+   // console.log(sum)
+    return sum;
 }
+   var sum =sumAll(1,50);
+   console.log(sum);
