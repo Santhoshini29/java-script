959ed62c5203d20ede64802fffaabf8c404532af
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 22:28:57 +0530

    updated

--- Primitives/callbacks.js
+++ Primitives/callbacks.js
@@ -35,3 +35,27 @@ function doArithmetic (firstEl,secondEl, arithmeticfn){
 
 var answer = doArithmetic(1,2,mul); 
                         console.log(answer);
+
+
+
+        //example to callbacks 
+        
+        // From this
+function doArithmetic(firstEl, secondEl, whatToDo) {
+	
+	if(whatToDo === "sum") {
+		var answer = sum(firstEl, secondEl);
+		return answer;
+	}
+
+	if(whatToDo === "multiply") {
+		var answer = multiply(firstEl, secondEl);
+		return answer;
+	}
+
+}
+
+// To this
+function doArithmetic(firstEl, secondEl, funToRun){
+	return funToRun(firstEl, secondEl);
+}
