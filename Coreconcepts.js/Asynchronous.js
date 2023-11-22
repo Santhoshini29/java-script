adb4019014094bb437ac693d3a8555f59f6dec9f
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 22:03:07 +0530

    changed

---
+++ Coreconcepts.js/Asynchronous.js
@@ -0,0 +1,28 @@
+ //sum function 
+
+ const fs = require('fs');
+ function sum(n){
+
+    let sum =0;
+    for (var i=0;i<n;i++){
+        sum +=1;
+    }
+     console.log (sum)
+ }
+  
+ 
+ //assigning ascync task --- read from a file using nodejs lib
+
+ function fileIsRead(err,fileContent){
+   sum(fileContent);
+   console.log("file is read gave final answer");
+ }
+ fs.readFile("hii.txt",'utf8',fileIsRead); 
+ 
+ 
+
+
+//other synchronous tasks 
+ console.log("Hello");
+ sum(21);
+ sum(20);
