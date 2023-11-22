2278a27be5173c5e21bed8bd7e9150202a7cb739
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 07:15:01 +0530

    updated

--- patterncreation/pyramid2.js
+++ patterncreation/pyramid2.js
@@ -1,1 +1,13 @@
-//full pyramid 
+//two four pattern
+function patternCreation(noofrows){
+
+    for (var i=0;i<noofrows;i++){
+        var str = " ";
+        for  (var j=0;j<i+1;j++){
+           str = str +"**";
+        }
+
+        console.log (str)
+    }
+}
+patternCreation(10);
