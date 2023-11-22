a1054075c76d2fcefdeea0642f64f0f9b00ab243
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 22:40:29 +0530

    changed

---
+++ Coreconcepts.js/types.js
@@ -0,0 +1,11 @@
+//Loosely typed example in JavaScript:
+
+// Can put "any type of data" in variable "iAmVariable"
+var iAmVariable = "Harkirat"
+iAmVariable = true
+iAmVariable = 5999
+
+//Strongly typed example in C++:
+// Can put "only specific type of data" in variable "iAmVariable"
+int iAmVariable = 5999;
+iAmVariable = "Harkirat" // ERROR !!! NOT POSSIBLE, only number
