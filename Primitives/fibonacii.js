efe6419ae8edc47e4952805124a4806973a96cff
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 05:39:27 +0530

    added

--- Primitives/fibonacii.js
+++ Primitives/fibonacii.js
@@ -1,15 +1,14 @@
 function generateFibonacci(n) {
-    let fibonacciSeries = [0, 1];
-  
-    for (let i = 2; i < n; i++) {
-      let nextNumber = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
-      fibonacciSeries.push(nextNumber);
-    }
-  
-    return fibonacciSeries;
+  let fibonacciSeries = [0, 1];
+
+  for (let i = 2; i < n; i++) {
+    let nextNumber = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+    fibonacciSeries.push(nextNumber);
   }
-  
-  // Example: Generate the first 10 Fibonacci numbers
-  let result = generateFibonacci(10);
-  console.log(result);
-  
+
+  return fibonacciSeries;
+}
+
+// Example: Generate the first 10 Fibonacci numbers
+let result = generateFibonacci(10);
+console.log(result);
