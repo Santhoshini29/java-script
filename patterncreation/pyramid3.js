f84c7ee70b889cc7f5cbe93dc774030320e98786
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-22 07:53:02 +0530

    added

--- patterncreation/pyramid3.js
+++ patterncreation/pyramid3.js
@@ -1,6 +1,25 @@
-//FULL PYRAMID 
+//FULL PYRAMID --using functions
+function starLine(noofrows){
+    var answer = " ";
+    for (var i =0;i<noofrows;i++){
+        answer = answer +"**";
+    }
+    console.log(answer);
+   
+}
+
+
 function starryPattern(noofrows){
 
+    for (var i =0;i<noofrows;i++){
+        starLine(i)
+    }
+
+   
 }
-var answer = starryPattern(5);
+
+
+
+
+var answer = starryPattern(15);
 console.log(answer);
